dad01c551135a7dddab13fe2c1649642b2b57b64
Peter Willsey <pwillsey@gmail.com>
2014-01-08 21:30:35 -0800

    Remove some dead code

--- Example Project/PWProgressView/PWMainView.m
+++ Example Project/PWProgressView/PWMainView.m
@@ -40,19 +40,6 @@
                                            300.0f,
                                            200.0f,
                                            25.0f);
-    
-//    self.view = [[UIView alloc] initWithFrame:CGRectMake(CGPointZero.x, CGPointZero.y, 320.0f, 560.0f)];
-//    self.view.backgroundColor = [UIColor redColor];
-//    
-//    self.progressView = [[PWProgressView alloc] initWithFrame:CGRectMake(50.0f, 50.0f, 100.0f, 100.0f)];
-//    [self.view addSubview:self.progressView];
-//    
-//    self.progressSlider = [[UISlider alloc] initWithFrame:CGRectMake(15.0f, 300.0f, 200.0f, 25.0f)];
-//    self.progressSlider.minimumValue = 0.0f;
-//    self.progressSlider.maximumValue = 1.0f;
-//    self.progressSlider.continuous = NO;
-//    [self.progressSlider addTarget:self action:@selector(setProgress:) forControlEvents:UIControlEventValueChanged];
-//    [self.view addSubview:self.progressSlider];
 }
 
 @end
